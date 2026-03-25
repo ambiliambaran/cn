94ada0ce2ea186463236164b3c8553e71c482c6d
Ambili Ambaran S <ambiliambaran191sa19@gmail.com>
2026-03-25 23:05:17 +0530

    DVR algorithm

--- dvr.c
+++ dvr.c
@@ -4,39 +4,75 @@
 #define INF 9999
 
 int main() {
-    int cost[MAX][MAX], dist[MAX][MAX], next[MAX][MAX];
     int n, i, j, k;
+    int cost[MAX][MAX], dist[MAX][MAX], via[MAX][MAX];
 
-    printf("Enter number of nodes: ");
+    printf("Distance Vector Routing\n");
+
+    printf("\nEnter number of nodes: ");
     scanf("%d", &n);
 
-    printf("Enter cost matrix:\n");
+    printf("\nEnter cost matrix:\n");
     for(i = 0; i < n; i++) {
         for(j = 0; j < n; j++) {
             scanf("%d", &cost[i][j]);
             dist[i][j] = cost[i][j];
-            next[i][j] = j;
+
+            if(i == j)
+                via[i][j] = i;
+            else if(cost[i][j] != INF)
+                via[i][j] = j;
+            else
+                via[i][j] = -1;
         }
     }
 
-    // Distance Vector Algorithm
+    // DVR Algorithm
     for(k = 0; k < n; k++) {
         for(i = 0; i < n; i++) {
             for(j = 0; j < n; j++) {
-                if(dist[i][j] > dist[i][k] + dist[k][j]) {
+
+                if(dist[i][k] != INF && dist[k][j] != INF &&
+                   dist[i][k] + dist[k][j] < dist[i][j]) {
+
                     dist[i][j] = dist[i][k] + dist[k][j];
-                    next[i][j] = next[i][k];
+                    via[i][j] = via[i][k];
                 }
             }
         }
     }
 
-    // Print routing table
+    // PRINT TABLE
     for(i = 0; i < n; i++) {
-        printf("\nRouting table for node %d:\n", i+1);
+        printf("\n\nRouting Table for Router %d\n", i+1);
+        printf("-------------------------------------------------\n");
         printf("Destination\tNext Hop\tDistance\n");
+        printf("-------------------------------------------------\n");
+
         for(j = 0; j < n; j++) {
-            printf("%d\t\t%d\t\t%d\n", j+1, next[i][j]+1, dist[i][j]);
+            if(via[i][j] == -1)
+                printf("%d\t\t-\t\tINF\n", j+1);
+            else
+                printf("%d\t\t%d\t\t%d\n", j+1, via[i][j]+1, dist[i][j]);
+        }
+        printf("-------------------------------------------------\n");
+    }
+
+    // PRINT PATHS
+    printf("\n\nPaths between nodes:\n");
+
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < n; j++) {
+            if(i != j && via[i][j] != -1) {
+                printf("Path from %d to %d: %d", i+1, j+1, i+1);
+
+                int u = i;
+                while(u != j) {
+                    u = via[u][j];
+                    printf(" -> %d", u+1);
+                }
+                printf("\n");
+            }
         }
     }
 
