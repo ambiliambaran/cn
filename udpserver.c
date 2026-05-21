32304aef9fa794ece226ff6c6d2ace0110030d11
Ambili Ambaran S <ambiliambaran191sa19@gmail.com>
2026-05-21 22:20:37 +0530

    udpserver

--- udpserver.c
+++ udpserver.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 
 int main() {
     int sockfd;
-    struct sockaddr_in server, client;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t addr_size;
     char buffer[1024];
-    socklen_t len = sizeof(client);
 
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 
@@ -17,34 +17,35 @@ int main() {
         exit(1);
     }
 
-    memset(&server, 0, sizeof(server));
-    memset(&client, 0, sizeof(client));
+    memset(&server_addr, 0, sizeof(server_addr));
+    memset(&client_addr, 0, sizeof(client_addr));
 
-    server.sin_family = AF_INET;
-    server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_port = htons(8080);
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(8080);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
 
-    if (bind(sockfd, (struct sockaddr *)&server, sizeof(server)) < 0) {
+    if (bind(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
         perror("Bind failed");
         exit(1);
     }
 
-    printf("UDP Server started...\n");
+    printf("Server waiting for data...\n");
+
+    addr_size = sizeof(client_addr);
 
     while (1) {
         memset(buffer, 0, sizeof(buffer));
 
         recvfrom(sockfd, buffer, sizeof(buffer) - 1, 0,
-                 (struct sockaddr *)&client, &len);
+                 (struct sockaddr*)&client_addr, &addr_size);
 
-        printf("Client: %s\n", buffer);
+        printf("Client: %s", buffer);
 
         printf("Server: ");
         fgets(buffer, sizeof(buffer), stdin);
-        buffer[strcspn(buffer, "\n")] = '\0';
 
         sendto(sockfd, buffer, strlen(buffer), 0,
-               (struct sockaddr *)&client, len);
+               (struct sockaddr*)&client_addr, addr_size);
     }
 
     close(sockfd);
